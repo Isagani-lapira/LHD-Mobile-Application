4b83fcbe0c15afc2ba3df3e794a27664975adc1b
Isagani Lapira <lapiraisagani@gmail.com>
2024-03-23 16:15:00 +0800

    Add: logout design

--- lhd_app/lib/widget/drawer.dart
+++ lhd_app/lib/widget/drawer.dart
@@ -13,33 +13,44 @@ class _CustomDrawerState extends State<CustomDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
+      child: Column(
         children: [
-          DrawerHeader(
-            child: Center(
-              child: Column(
-                children: [
-                  const Expanded(
-                    child: CircleAvatar(
-                      radius: 80.0,
-                      backgroundImage: NetworkImage(
-                          'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg'),
+          Expanded(
+            flex: 1,
+            child: ListView(
+              children: [
+                DrawerHeader(
+                  child: Center(
+                    child: Column(
+                      children: [
+                        const Expanded(
+                          child: CircleAvatar(
+                            radius: 80.0,
+                            backgroundImage: NetworkImage(
+                                'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg'),
+                          ),
+                        ),
+                        Text(
+                          'Isagani T. Lapira Jr',
+                          style: AppTheme.lightTheme.textTheme.bodyMedium!
+                              .copyWith(fontWeight: FontWeight.w600),
+                        ),
+                      ],
                     ),
                   ),
-                  Text(
-                    'Isagani T. Lapira Jr',
-                    style: AppTheme.lightTheme.textTheme.bodyMedium!
-                        .copyWith(fontWeight: FontWeight.w600),
-                  ),
-                ],
-              ),
+                ),
+                for (var menu in kMenuItem)
+                  ListTile(
+                    leading: menu['icon'],
+                    title: Text(menu['title']),
+                  )
+              ],
             ),
           ),
-          for (var menu in kMenuItem)
-            ListTile(
-              leading: menu['icon'],
-              title: Text(menu['title']),
-            )
+          const ListTile(
+            leading: Icon(Icons.logout_outlined),
+            title: Text('Sign out'),
+          )
         ],
       ),
     );
