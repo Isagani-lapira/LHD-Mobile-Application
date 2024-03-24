95a1c3888bfaa7d1b0ce2b1e15d5bc5b376f9c19
Isagani Lapira <lapiraisagani@gmail.com>
2024-03-24 15:34:12 +0800

    Add: Firebase connectivity

--- lhd_app/lib/main.dart
+++ lhd_app/lib/main.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:lhd_app/screens/homescreen.dart';
 import 'package:lhd_app/screens/landingscreen.dart';
 import 'package:lhd_app/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const LHDApp());
 }
 
