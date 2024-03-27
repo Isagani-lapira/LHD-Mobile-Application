d3815515b46ebff882b6377f5e1c98da1e20b3e5
Isagani Lapira <lapiraisagani@gmail.com>
2024-03-27 16:03:35 +0800

    Add: fullscreen dialog

--- lhd_app/lib/widget/sipsseries.dart
+++ lhd_app/lib/widget/sipsseries.dart
@@ -12,7 +12,6 @@ class SIPThumbnail extends StatefulWidget {
 class _SIPThumbnailState extends State<SIPThumbnail> {
   late String videoId;
   late YoutubePlayerController _controller;
-
   @override
   void initState() {
     super.initState();
@@ -23,6 +22,67 @@ class _SIPThumbnailState extends State<SIPThumbnail> {
         flags: const YoutubePlayerFlags(
           autoPlay: false,
         ));
+
+    _controller.addListener(_onFullScreen);
+  }
+
+  void _onFullScreen() {
+    if (_controller.value.isFullScreen) {
+      _controller.pause();
+      _controller.toggleFullScreenMode();
+      showDialog(
+          context: context,
+          builder: (context) {
+            return FullScreenVideo(videoID: videoId);
+          });
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return YoutubePlayer(
+      controller: _controller,
+      showVideoProgressIndicator: true,
+    );
+  }
+}
+
+// dialog full screen
+class FullScreenVideo extends StatefulWidget {
+  final String videoID;
+  const FullScreenVideo({super.key, required this.videoID});
+
+  @override
+  State<FullScreenVideo> createState() => _FullScreenVideoState();
+}
+
+class _FullScreenVideoState extends State<FullScreenVideo> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+        initialVideoId: widget.videoID,
+        flags: const YoutubePlayerFlags(
+          autoPlay: true,
+        ));
+
+    _controller.toggleFullScreenMode(); //full screen automatically
+    _controller.addListener(closeFullScreen);
+  }
+
+  void closeFullScreen() {
+    if (!_controller.value.isFullScreen) {
+      Navigator.pop(
+          context); //close video player once click the minimize screen
+    }
   }
 
   @override
